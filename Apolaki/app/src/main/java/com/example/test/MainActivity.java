a2b4098587a2994511e0301d76c0fd66fc362fa4
JoshuaMina <joshua.minacambronero@gmail.com>
2023-06-13 13:52:37 +0800

    Second commit, inayos lang ung pers page pero may code na papuntang search page

--- Apolaki/app/src/main/java/com/example/test/MainActivity.java
+++ Apolaki/app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.SearchEvent;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    public ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (ImageButton) findViewById(R.id.searchbtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Search.class);
+                startActivity(intent);
+            }
+        });
     }
 }
